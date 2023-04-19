133bb495eaa2da483d921eb85dff34d1d9a84b42
Hugo Richard <hugo.richard@datamedcare.com>
2023-04-19 14:13:05 +0200

    📦️add swiperjs + update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import node from "@astrojs/node";
-
 import svelte from "@astrojs/svelte";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), svelte()],
+  integrations: [tailwind(), solidJs(), svelte(), compress()],
   output: "server",
   adapter: node({
     mode: "standalone"
