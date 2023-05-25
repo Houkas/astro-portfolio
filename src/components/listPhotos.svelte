ab14d5d15d0fef3bef850ddc00414bab14f359c7
Hugo Richard <hugo.richard@datamedcare.com>
2023-05-25 14:09:51 +0200

    ListPhotos -> use compressed photos

--- src/components/listPhotos.svelte
+++ src/components/listPhotos.svelte
@@ -8,7 +8,9 @@
       await fetch('https://api.hugo-richard-work.fr/api/photos')
       .then(r => r.json())
       .then(respPhotos => {
-        photos = respPhotos.data;
+
+        photos = respPhotos.data.filter((d: Photo) => d.attributes.lien_photo_compressed !== null);
+        console.log(photos)
       });
     });
 </script>
@@ -17,7 +19,7 @@
     <h1 class='text-center uppercase m-5 title-hugo'> Photos </h1>
     <div class="flex flex-row flex-wrap justify-center max-w-[750px]">
         {#each photos as photo}
-            <img src={photo.attributes.lien_photo} alt="test" class="max-w-[120px] md:max-w-[225px] m-2">
+            <img src={photo.attributes.lien_photo_compressed} alt="test" class="max-w-[120px] md:max-w-[225px] m-2">
         {/each}
     </div>
 
